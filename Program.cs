c3d48e03e6e39bcc4705266fea742301f5d5eccd
ALEX K. KEMBOI <AKEMBOI@fintech-group.com>
2023-06-12 15:17:57 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+
+public class SayHi
+{
+    public static void PrintGreeting(string name)
+    {
+        Console.WriteLine("Hello, " + name + "!");
+    }
+    public static void findSum(int a,int b)
+    {
+        Console.WriteLine(a+b);
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string name = "John"; // Replace with the desired name
+        int a = 1;
+        int b = 2;
+        SayHi.PrintGreeting(name);
+        SayHi.findSum(a, b);
+    }
+}
+
