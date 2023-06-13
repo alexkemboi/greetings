c5ce0efab1073b0fa979c6775832f5a73385fa0a
ALEX K. KEMBOI <AKEMBOI@fintech-group.com>
2023-06-13 16:27:28 +0300

    added condition to check if number entered is odd or even

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-//Console.WriteLine("Hello, World!");
+﻿
 using System;
 
 public class SayHi
@@ -15,14 +14,45 @@ public class SayHi
 }
 
 public class Program
-{
-    public static void Main(string[] args)
+{    public static void Main(string[] args)
     {
-        string name = "John"; // Replace with the desired name
+        // Prompt the user for their name
+        Console.Write("Enter your name: ");
+        string? name = "";
+        name = Console.ReadLine();
+        // Prompt the user for their name
+        Console.Write("Enter the first number:");
         int a = 1;
         int b = 2;
-        SayHi.PrintGreeting(name);
+        string? input = Console.ReadLine();
+        int parsedValue;
+
+        if (int.TryParse(input, out parsedValue))
+        {
+            b = parsedValue;
+        }
+        else
+        {
+            // Handle the case where the input cannot be parsed to an int
+        }
+
+        // Print a greeting with the user's name
+        Console.WriteLine($"Hello, {name}!");
+        // Wait for user input before closing the console window
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey(); // Replace with the desired name
+        if (name != null)
+        {
+            SayHi.PrintGreeting(name);
+        }
         SayHi.findSum(a, b);
-    }
+        if (b % 2 == 0)
+        {
+            Console.WriteLine("even");
+        }
+        else{
+            Console.WriteLine("odd");
+        }
+}
 }
 
